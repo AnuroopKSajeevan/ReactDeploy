62def6287a7c4fcbb076e303f2a012617ce4653b
=Anuroop <anuroopks7911@gmail.com>
2024-08-13 14:12:43 +0530

    first commit

--- src/jest.config.js
+++ src/jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   testEnvironment: "jsdom",
   transform: {
-    "^.+\\.jsx?$": "babel-jest",
+    "^.+\\.[jt]sx?$": "babel-jest",
   },
+  moduleFileExtensions: ["js", "jsx", "ts", "tsx"],
 };
