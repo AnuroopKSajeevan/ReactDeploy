0a5a8fd72b303de0f180ace5619ab9a56a61ce59
=Anuroop <anuroopks7911@gmail.com>
2024-08-13 14:23:22 +0530

    first commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+// src/App.test.js
+import { render, screen, waitFor } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect"; // For custom matchers
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders Project List heading", async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  // Wait for the component to finish loading the data
+  await waitFor(() => {
+    // Check if the "Project List" heading is present
+    const headingElement = screen.getByText(/project list/i);
+    expect(headingElement).toBeInTheDocument();
+  });
 });
